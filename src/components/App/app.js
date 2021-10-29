96141109fcfbc04bd5e20b6b65f853dd67921539
sorok <sorokivroma@gmail.com>
2021-10-29 18:14:15 +0300

    Update project

---
+++ src/components/App/app.js
@@ -0,0 +1,8 @@
+
+export default App =>{
+    return(
+        <div className="app">
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+//stylesheet
+import './css/index.css'
+//import app
+import App from './components/App/app'
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
